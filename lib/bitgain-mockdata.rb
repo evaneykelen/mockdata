8f45d3ee21a707b456f92c7cc0c3febc158b5af6
Erik van Eykelen <erik.van.eykelen@bitgain.com>
2011-01-18 14:59:47 +0100

    README

--- lib/bitgain-mockdata.rb
+++ lib/bitgain-mockdata.rb
@@ -7,21 +7,5 @@ require 'random/words'
 
 module Bitgain
   module Mockdata
-    # Examples:
-    #
-    # Bitgain::Mockdata::Locations.pick 
-    # => Farmington, 36.74108512094412, -108.1658935546875
-    #
-    # Bitgain::Mockdata::Names.project_name
-    # => Northernmost Galaxy
-    #
-    # Bitgain::Mockdata::Numbers.random_number_array(1..10,3)
-    # => [9,1,4]
-    #
-    # Bitgain::Mockdata::TimeZones.pick
-    # => Mountain Time (US &amp; Canada)
-    #
-    # Bitgain::Mockdata::Words.some(2..10)
-    # => beam scan grown tape course thimble newscast main asleep
   end
 end
