a0c30b5aad18c719a64503cfe4e13598fa68645f
Erik van Eykelen <erik.van.eykelen@bitgain.com>
2011-01-18 14:43:35 +0100

    Example usage

--- lib/bitgain-mockdata.rb
+++ lib/bitgain-mockdata.rb
@@ -7,10 +7,21 @@ require 'random/words'
 
 module Bitgain
   module Mockdata
-    puts Bitgain::Mockdata::Locations.pick
-    puts Bitgain::Mockdata::Names.project_name
-    puts Bitgain::Mockdata::Numbers.random_number_array(1..10,3)
-    puts Bitgain::Mockdata::TimeZones.pick
-    puts Bitgain::Mockdata::Words.some(2..10)
+    # Examples:
+    #
+    # Bitgain::Mockdata::Locations.pick 
+    # => Farmington, 36.74108512094412, -108.1658935546875
+    #
+    # Bitgain::Mockdata::Names.project_name
+    # => Northernmost Galaxy
+    #
+    # Bitgain::Mockdata::Numbers.random_number_array(1..10,3)
+    # => [9,1,4]
+    #
+    # Bitgain::Mockdata::TimeZones.pick
+    # => Mountain Time (US &amp; Canada)
+    #
+    # Bitgain::Mockdata::Words.some(2..10)
+    # => beam scan grown tape course thimble newscast main asleep
   end
 end
