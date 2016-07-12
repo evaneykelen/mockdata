7a2accae0a112687f394ffccde2748514e56dc41
Erik van Eykelen <erik@clubcollect.com>
2016-07-12 20:32:42 +0200

    Add Dutch names

--- test/test_people.rb
+++ test/test_people.rb
@@ -4,10 +4,13 @@ require 'mockdata/people'
 class MockdataTest < Minitest::Test
   def test_people
     refute_nil Mockdata::People.first_and_last_name
-    assert Mockdata::People::FIRST_NAMES.include?     Mockdata::People.first_name
-    assert Mockdata::People::LAST_NAMES.include?      Mockdata::People.last_name
-    assert Mockdata::People::SUPERHERO_NAMES.include? Mockdata::People.superhero
-    assert Mockdata::People::ELF_NAMES.include?       Mockdata::People.elf
-    assert Mockdata::People::ROBOT_NAMES.include?     Mockdata::People.robot
+    refute_nil Mockdata::People.dutch_first_and_last_name
+    assert Mockdata::People::FIRST_NAMES.include?             Mockdata::People.first_name
+    assert Mockdata::People::LAST_NAMES.include?              Mockdata::People.last_name
+    assert Mockdata::People::FIRST_NAMES_NETHERLANDS.include? Mockdata::People.dutch_first_name
+    assert Mockdata::People::LAST_NAMES_NETHERLANDS.include?  Mockdata::People.dutch_last_name
+    assert Mockdata::People::SUPERHERO_NAMES.include?         Mockdata::People.superhero
+    assert Mockdata::People::ELF_NAMES.include?               Mockdata::People.elf
+    assert Mockdata::People::ROBOT_NAMES.include?             Mockdata::People.robot
   end
 end
